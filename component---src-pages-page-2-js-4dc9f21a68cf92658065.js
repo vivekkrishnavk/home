eadd4d510ca371e74ddb31584fb17778a85c175c
Vivek Krishna <vivek@Viveks-MacBook-Pro.local>
2019-10-18 00:36:48 +0530

    Updates

---
+++ component---src-pages-page-2-js-4dc9f21a68cf92658065.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{223:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(152),c=a(221),r=a(220);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-4dc9f21a68cf92658065.js.map
